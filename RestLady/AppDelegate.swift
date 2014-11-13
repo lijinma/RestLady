6db6ee4e6a0f2e16686de65b5ce0398ec6d7ea10
Jinma li <lijinma@126.com>
2014-11-13 18:48:57 +0800

    Initial Commit

---
+++ RestLady/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RestLady
+//
+//  Created by Li JinMa on 14/11/13.
+//  Copyright (c) 2014年 Li JinMa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RestLady/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RestLady
+//
+//  Created by Li JinMa on 14/11/13.
+//  Copyright (c) 2014年 Li JinMa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
